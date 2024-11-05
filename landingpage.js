68700c6763b623f3ae5fca0a5076d74e521d6106
isMoon <naelprakasa@gmail.com>
2024-11-05 21:17:58 +0700

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,58 @@
+// $(document).ready(function(){
+//     // Add smooth scrolling to all links
+//     $("a").on('click', function(event) {
+  
+//       // Make sure this.hash has a value before overriding default behavior
+//       if (this.hash !== "") {
+//         // Prevent default anchor click behavior
+//         event.preventDefault();
+  
+//         // Store hash
+//         var hash = this.hash;
+  
+//         // Using jQuery's animate() method to add smooth page scroll
+//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         }, 800, function(){
+     
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         });
+//       } // End if
+//     });
+//   });
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
